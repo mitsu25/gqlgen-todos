334fefef04782317c43bd86c9cf3aa9e68f333bf
mitsu25 <m.nakano65@gmail.com>
2020-07-11 15:25:01 +0900

    done tutorial

---
+++ graph/resolver.go
@@ -0,0 +1,14 @@
+package graph
+
+//go:generate go run github.com/99designs/gqlgen
+import (
+	"github.com/mitsu25/gqlgen-todos/graph/model"
+)
+
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+type Resolver struct{
+	todos []*model.Todo
+}
